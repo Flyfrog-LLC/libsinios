720ed597477d28d2ec99ba89f4b6cd28b7124494
InfiniteFalltrough <golubenkovvs@gmail.com>
2025-08-21 12:24:43 +0700

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "Libbox",
+    platforms: [
+        .iOS(.v15),
+        .macCatalyst(.v15)
+    ],
+    products: [
+        .library(
+            name: "Libbox",
+            targets: ["Libbox"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Libbox",
+            url: "https://github.com/Flyfrog-LLC/libsinios/releases/download/v0.0.1/Libbox.xcframework.zip",
+            checksum: ""
+        )
+    ]
+)
