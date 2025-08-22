2b04ea58b92a64969353964101e0dd2f3cd0a7ec
InfiniteFalltrough <golubenkovvs@gmail.com>
2025-08-22 15:48:15 +0700

    manual for test v1.12.0 + dirty trick

--- Package.swift
+++ Package.swift
@@ -9,15 +9,34 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "Libbox",
-            targets: ["Libbox"]
+            name: "LibboxApp",
+            targets: ["LibboxApp"]
         ),
+        .library(
+            name: "LibboxTunnel",
+            targets: ["LibboxTunnel"]
+        )
     ],
     targets: [
+        // raw binary
         .binaryTarget(
-            name: "Libbox",
+            name: "LibboxBinary",
             url: "https://github.com/Flyfrog-LLC/libsinios/releases/download/v1.12.0/Libbox.xcframework.zip",
             checksum: "2310e6009aaa345a61cea373697acad98c21de560dee494d06ef22c730cb3e70"
+        ),
+
+        // dirty trick
+        .target(
+            name: "LibboxApp",
+            dependencies: ["LibboxBinary"],
+            path: "Wrappers/App",
+            sources: ["Dummy.swift"]
+        ),
+        .target(
+            name: "LibboxTunnel",
+            dependencies: ["LibboxBinary"],
+            path: "Wrappers/Tunnel",
+            sources: ["Dummy.swift"]
         )
     ]
 )
---
+++ Wrappers/Tunnel/Dummy.swift
@@ -0,0 +1,10 @@
+//
+//  Dummy.swift
+//  libsinios
+//
+//  Created by Viktor Golubenkov on 8/22/25.
+//
+
+import Foundation
+
+public enum _LibboxTunnel {}
